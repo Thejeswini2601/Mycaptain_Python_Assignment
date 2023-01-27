d52e81cd1e40351e3bb4a1b627842469cebb0a31
THEJESWINI <122153770+Thejeswini2601@users.noreply.github.com>
2023-01-27 20:43:31 +0530

    Add files via upload

---
+++ print positive numbers.py
@@ -0,0 +1,19 @@
+
+list1=[12,-7,5,64,-14]
+for i in list1:
+    if i>0:
+        print(i,end=" ")
+
+
+
+        
+##############################
+
+
+
+        
+list2=[12,14,-95,3]
+for i in list2:
+    if i<0:
+        list2.remove(i)
+print(list2)
